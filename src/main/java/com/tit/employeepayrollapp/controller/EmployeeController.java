af4e2c61513b98bca88514ab6c6fdb6a3c5fcf12
AdityaRaj00007 <rajgupta49102@gmail.com>
2025-02-24 11:23:40 +0530

    [Aditya] Add . Added various Http Methods

--- src/main/java/com/tit/employeepayrollapp/controller/EmployeeController.java
+++ src/main/java/com/tit/employeepayrollapp/controller/EmployeeController.java
@@ -18,13 +18,13 @@ class EmployeeController {
 
     @GetMapping("/{id}")
     public Employee getEmployeeById(@PathVariable Long id) { return service.getEmployeeById(id); }
-
+    // post http method
     @PostMapping
     public Employee addEmployee(@RequestBody Employee employee) { return service.addEmployee(employee); }
-
+    // put https method
     @PutMapping("/{id}")
     public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee) { return service.updateEmployee(id, employee); }
-
+    // delete http method
     @DeleteMapping("/{id}")
     public void deleteEmployee(@PathVariable Long id) { service.deleteEmployee(id); }
 }
