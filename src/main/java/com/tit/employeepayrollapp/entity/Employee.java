b4926cc2631e6d13635bea52c1c3c2c0726c50e9
AdityaRaj00007 <rajgupta49102@gmail.com>
2025-02-28 16:37:30 +0530

    [Aditya] Add . Enable saving Employee Payroll data to MySQL DB

--- src/main/java/com/tit/employeepayrollapp/entity/Employee.java
+++ src/main/java/com/tit/employeepayrollapp/entity/Employee.java
@@ -1,19 +1,18 @@
 package com.tit.employeepayrollapp.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 @Entity
 @Data
 @Slf4j
+@Table(name = "Employee")
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "name")
     private String name;
     private double salary;
     private String department;
